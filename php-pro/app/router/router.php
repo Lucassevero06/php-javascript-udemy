110193a3eb0c59aa7a484a615a4706c0b6f5ef70
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-08-30 21:10:13 -0400

    criando funções para tra balhar com routes fixas e dinamicas

--- php-pro/app/router/router.php
+++ php-pro/app/router/router.php
@@ -2,18 +2,36 @@
 function routes() {
     return require 'routes.php';
 }
-
+//trabalhando com as uri exatas/fixas
 function exactMatchUriInArrayRoutes($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
 }
-
+//trabalhando com as uri dinamicas
+function regularExpressionMatchArrayRoutes($uri, $routes) {
+    return array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+//trabalhando com as duas funçoes juntas
 function router() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //capturando a uri
     $routes = routes();
     //verificando se a rota existe
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if (empty($matchedUri)) {
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+    }
+
+    var_dump($matchedUri);
+    die();
 }
--- php-pro/app/router/routes.php
+++ php-pro/app/router/routes.php
@@ -2,4 +2,5 @@
 return [
     '/' => 'Home@index',
     '/user/create' => 'User@create',
+    '/user/[0-9]+' => 'User@index',
 ];
