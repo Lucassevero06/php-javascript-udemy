6da3fe91f25a91ee49aab5b094a4caf92f2e4657
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-08-30 21:31:55 -0400

    estudando php puro

--- php-pro/app/router/router.php
+++ php-pro/app/router/router.php
@@ -21,15 +21,33 @@ function regularExpressionMatchArrayRoutes($uri, $routes) {
         ARRAY_FILTER_USE_KEY
     );
 }
+
+function params($uri, $matchedUri) {
+    if (!empty($matchedUri)) {
+        $matchedToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            explode('/', ltrim($uri, '/')),
+            explode('/', ltrim($matchedToGetParams, '/'))
+        );
+    }
+    return [];
+}
+
 //trabalhando com as duas funçoes juntas
 function router() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //capturando a uri
+
     $routes = routes();
+
     //verificando se a rota existe
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
 
     if (empty($matchedUri)) {
         $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+
+        $params = params($uri, $matchedUri);
+        var_dump($params);
+        die();
     }
 
     var_dump($matchedUri);
